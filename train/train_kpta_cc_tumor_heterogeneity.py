a3a67b9bf3e880df8439ea28b2f6f4d6030afcf6
wdn <2013358857@qq.com>
2026-06-23 12:00:38 +0800

    log

--- train/train_kpta_cc_tumor_heterogeneity.py
+++ train/train_kpta_cc_tumor_heterogeneity.py
@@ -9,7 +9,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-DEFAULT_CONFIG = PROJECT_ROOT / "configs" / "kpta_25d_net_cc_tumor_heterogeneity_17ch.yaml"
+DEFAULT_CONFIG = PROJECT_ROOT / "configs" / "kpta_25d_net_cc_tumor_heterogeneity_17ch-v2.yaml"
 
 
 if __name__ == "__main__":
